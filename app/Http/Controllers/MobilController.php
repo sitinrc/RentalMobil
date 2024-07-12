e10088bf707206ea9cf0301f753ec27b5a107d00
Shafira <azzahranuhrova@gmail.com>
2024-07-12 15:47:08 +0700

    shafira menambahkan controller

--- app/Http/Controllers/MobilController.php
+++ app/Http/Controllers/MobilController.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Http\Controllers;
 
-use App\Models\Mobil;
 use Illuminate\Http\Request;
+use App\Models\Mobil;
 
 class MobilController extends Controller
 {
@@ -13,11 +12,61 @@ class MobilController extends Controller
         return view('mobil.index', compact('mobils'));
     }
 
-    public function destroy($id)
+    public function create()
+    {
+        return view('mobil.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            
+            'nama_mobil' => 'required|string|max:255',
+            'merk' => 'required|string|max:255',
+            'tahun' => 'required',
+        ]);
+
+        Mobil::create($request->all());
+
+        return redirect()->route('mobil.index')->with('success', 'Mobil created successfully.');
+    }
+
+    public function show(Mobil $mobil)
+    {
+        return view('mobil.show', compact('mobil'));
+    }
+
+    public function edit($id)
     {
         $mobil = Mobil::findOrFail($id);
+        return view('mobil.edit', compact('mobil'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama_mobil' => 'required|string|max:255',
+            'merk' => 'required|string|max:255',
+            'tahun' => 'required|integer',
+            'harga_sewa_per_hari' => 'required|numeric',
+        ]);
+
+        $mobil = Mobil::findOrFail($id);
+        $mobil->nama_mobil = $request->nama_mobil;
+        $mobil->merk = $request->merk;
+        $mobil->tahun = $request->tahun;
+        $mobil->harga_sewa_per_hari = $request->harga_sewa_per_hari;
+        $mobil->save();
+
+        return redirect()->route('mobil.index')->with('success-status', 'Mobil berhasil diperbarui.');
+    }
+
+
+    public function destroy(Mobil $mobil)
+    {
         $mobil->delete();
 
-        return redirect()->route('mobils.index')->with('success', 'Mobil berhasil dihapus');
+        return redirect()->route('mobil.index')
+                         ->with('success', 'mobils berhasil dihapus.');
     }
-}  
+}
--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Pelanggan;
 use Illuminate\Http\Request;
+use App\Models\Pelanggan;
 
 class PelangganController extends Controller
 {
@@ -13,11 +13,58 @@ class PelangganController extends Controller
         return view('pelanggan.index', compact('pelanggans'));
     }
 
-    public function destroy($id)
+    public function create()
+    {
+        return view('pelanggan.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'alamat' => 'required|string|max:255',
+            'no_telepon' => 'required|string|max:15',
+            'email' => 'required|string|email|max:255|unique:pelanggans',
+        ]);
+
+        Pelanggan::create($request->all());
+
+        return redirect()->route('pelanggan.index')->with('success', 'Pelanggan created successfully.');
+    }
+
+    public function edit($id)
     {
         $pelanggan = Pelanggan::findOrFail($id);
+        return view('pelanggan.edit', compact('pelanggan'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nama' => 'required|string|max:255',
+            'alamat' => 'required|string',
+            'no_telepon' => 'required|string|max:15',
+            'email' => 'required|email|max:255',
+        ]);
+
+        $pelanggan = Pelanggan::findOrFail($id);
+        $pelanggan->nama = $request->nama;
+        $pelanggan->alamat = $request->alamat;
+        $pelanggan->no_telepon = $request->no_telepon;
+        $pelanggan->email = $request->email;
+        $pelanggan->save();
+
+        return redirect()->route('pelanggan.index')->with('success-status', 'Pelanggan berhasil diperbarui.');
+    }
+
+
+    public function destroy(Pelanggan $pelanggan)
+    {
         $pelanggan->delete();
 
-        return redirect()->route('pelanggans.index')->with('success', 'Pelanggan berhasil dihapus');
+        return redirect()->route('pelanggan.index')
+                         ->with('success', 'Pelanggan berhasil dihapus.');
     }
+
+    
 }
